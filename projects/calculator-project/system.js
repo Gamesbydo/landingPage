352497d587f5d367c4f12d3f223f2b68bb12a81a
Mikulas <gamesbydo@gmail.com>
2023-12-07 15:50:01 +0100

    included wrong system.js file

--- projects/calculator-project/system.js
+++ projects/calculator-project/system.js
@@ -1,83 +1,97 @@
-/*
-TODO
-- add parentheses
-- add option to delete
-- add history? -> maybe like somewhere to the side
-*/
-let num1 = "";
-let num2 = "";
-let operation;
-let result = 0;
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const display = document.querySelector(".display");
 
-function putNumberOnScreen(
-    clickedButton,
-    displayParagraph = document.getElementById("displayText")
-) {
-    if (typeof clickedButton === "number" && typeof num1 === "string") {
-        num1 += String(clickedButton);
-        displayParagraph.textContent = String(num1);
-    } else if (typeof clickedButton === "number" && typeof num1 === "number") {
-        num2 += String(clickedButton);
-        displayParagraph.textContent += String(clickedButton);
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
     } else {
-        if (clickedButton !== "=") {
-            displayParagraph.textContent += String(clickedButton);
-        }
-        switch (clickedButton) {
-            case "=":
-                switch (operation) {
-                    case "+":
-                        result = num1 + Number(num2);
-                        displayParagraph.textContent = String(result);
-                        num1 = "";
-                        num2 = "";
-                        result = 0;
-                        break;
-                    case "-":
-                        result = num1 - Number(num2);
-                        displayParagraph.textContent = String(result);
-                        num1 = "";
-                        num2 = "";
-                        result = 0;
-                        break;
-                    case "*":
-                        result = num1 * Number(num2);
-                        displayParagraph.textContent = String(result);
-                        num1 = "";
-                        num2 = "";
-                        result = 0;
-                        break;
-                    case "/":
-                        result = num1 / Number(num2);
-                        displayParagraph.textContent = String(result);
-                        num1 = "";
-                        num2 = "";
-                        result = 0;
-                        break;
-                }
-                break;
-            case "*":
-                num1 = Number(num1);
-                operation = "*";
-                break;
-            case "+":
-                num1 = Number(num1);
-                operation = "+";
-                break;
-            case "-":
-                num1 = Number(num1);
-                operation = "-";
-                break;
-            case "/":
-                num1 = Number(num1);
-                operation = "/";
-                break;
-            case "square":
-                num1 = Number(num1);
-                result = num1 * num1;
-                displayParagraph.textContent = String(result);
-                num1 = "";
-                break;
-        }
+        handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber(value) {
+    if (buffer === "0") {
+        buffer = value;
+    } else {
+        buffer += value;
     }
 }
+
+function handleMath(value) {
+    if (buffer === "0") {
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+
+    buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === "+") {
+        runningTotal += intBuffer;
+    } else if (previousOperator === "-") {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === "*") {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleSymbol(value) {
+    switch (value) {
+        case "C":
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case "=":
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = +runningTotal;
+            runningTotal = 0;
+            break;
+        case "<":
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case "+":
+        case "-":
+        case "*":
+        case "÷":
+            handleMath(value);
+            break;
+    }
+}
+
+function rerender() {
+    display.innerText = buffer;
+}
+
+function init() {
+    const buttons = document.querySelectorAll(".button-class");
+
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function (event) {
+            buttonClick(event.target.innerText);
+        });
+    });
+}
+
+init();
